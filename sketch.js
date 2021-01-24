f8d84f832fc3d2dadda4672034bd7c7792cb7ffc
behlool20077 <77536212+behlool20077@users.noreply.github.com>
2021-01-24 16:47:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -85,4 +85,20 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=06 && hour<=19){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
 }
